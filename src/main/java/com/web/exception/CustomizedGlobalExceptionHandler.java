3102c48263b723d91a8d875294b49c7bee190455
AH_L <ihpb1727@gmail.com>
2024-04-27 00:31:35 +0900

    exceptionResponse에 맞춰서 예외처리 #8

--- src/main/java/com/web/exception/CustomizedGlobalExceptionHandler.java
+++ src/main/java/com/web/exception/CustomizedGlobalExceptionHandler.java
@@ -8,15 +8,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 public class CustomizedGlobalExceptionHandler{
 	
 	@Autowired
 	MessageSource ms;
-
+	
+	//NotValid 검증 예외처리
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	ExceptionResponse validationExceptionHandler(MethodArgumentNotValidException e) {		
+	ExceptionResponse validationExceptionHandler(MethodArgumentNotValidException e) {
 		
 		ExceptionResponse exceptionResponse = ExceptionResponse.builder()
 				.status(e.getStatusCode().value())
@@ -26,10 +28,37 @@ public class CustomizedGlobalExceptionHandler{
 				.build();
 		
 		return exceptionResponse;
-    }
+	}
 	
+	//예외처리 response
+	public ExceptionResponse getExceptionResponse(int status, Exception e) {
+		
+		ExceptionResponse exceptionResponse = ExceptionResponse.builder()
+				.status(status)
+				.timestamp(LocalDateTime.now())
+				.errorClass(e.getClass().getName())
+				.errMsg(e.getMessage())
+				.build();
+		
+		return exceptionResponse;
+	}
+	
+	//NullPoint 예외
+	@ExceptionHandler(NullPointerException.class)
+	ExceptionResponse nullPointerExceptionHandler(NullPointerException e) {
+		return getExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e);
+	}
+	
+	//NoResourceFoundException 예외
+	@ExceptionHandler(NoResourceFoundException.class)
+	ExceptionResponse noResourceFoundExceptionHandler(NoResourceFoundException e) {
+		return getExceptionResponse(HttpStatus.BAD_REQUEST.value(), e);
+	}
+	
+	
+	//기타 예외
 	@ExceptionHandler
-	ExceptionResponse allExceptionHandler(Exception e) {
+	ExceptionResponse otherExceptionHandler(Exception e) {
 		
 		ExceptionResponse exceptionResponse = ExceptionResponse.builder()
 				.status(HttpStatus.INTERNAL_SERVER_ERROR.value())
